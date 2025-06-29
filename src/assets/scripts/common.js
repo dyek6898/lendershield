e467150b169f9206aa8e338259ab6d054e01bac7
jeyoung <dyek6898@etribe.co.kr>
2025-06-29 23:40:29 +0900

    알림 및 유저정보 선택 시 스크립트 추가

--- src/assets/scripts/common.js
+++ src/assets/scripts/common.js
@@ -1,5 +1,32 @@
-etUI.utils.ready(function() {
+etUI.utils.ready(function () {
   console.log('document ready');
-
   etUI.initUI();
-})
+
+  // [공통] 클릭 이벤트
+  etUI.pages.clickEvent = function () {
+    const alarm = document.querySelector('.btn-alarm');
+    const userSelect = document.querySelector('.btn-select');
+
+    alarm.addEventListener('click', function (e) {
+      const target = e.target.closest('.btn-area').querySelector('.alarm-info');
+      this.classList.toggle('on');
+
+      if(this.classList.contains('on')){
+        target.style.display = "block";
+      } else target.style.display = "none";
+    });
+
+    userSelect.addEventListener('click', function (e) {
+      const target = e.target.closest('.info-area').querySelector('.select-list');
+      this.classList.toggle('on');
+
+      if(this.classList.contains('on')){
+        target.style.display = "block";
+      } else target.style.display = "none";
+    });
+  };
+
+  document.fonts.ready.then(() => {
+    etUI.pages.clickEvent();
+  });
+});
